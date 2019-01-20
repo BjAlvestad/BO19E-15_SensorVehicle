2734d8aae3703ddb45b5baedb3e2a4f814a53154
BjAlvestad <40029656+BjAlvestad@users.noreply.github.com>
2019-01-20 11:34:47 +0100

    Adds Interface IDistance for distance measurements
    The interface ensures that the all who implements it at least contain a Fwd, Left and Right property as well as Min and Max range, and Resolution.
    The intention is that the Distance class will accept classes implementing IDistance in its constructor, and will blend all sensor data it receives.

---
+++ SensorVehicle-main/VehicleEquipment/DistanceMeasurement/IDistance.cs
@@ -0,0 +1,12 @@
+﻿namespace VehicleEquipment.DistanceMeasurement
+{
+    internal interface IDistance
+    {
+        float Fwd { get; }
+        float Left { get; }
+        float Right { get; }        
+        float MinRange { get; }        
+        float MaxRange { get; }        
+        float Resolution { get; }        
+    }
+}
