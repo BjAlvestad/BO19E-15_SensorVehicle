f3c46b7ebc9ec09dcd3d7712e95e8e5a94c90bd8
BjAlvestad <40029656+BjAlvestad@users.noreply.github.com>
2019-01-20 11:49:02 +0100

    Adds classes for Ultrasound (not implemented yet)
    The UltrasoundReceiver class is intended to receive distance measurement from the arduino board.
    
    The UltrasoundDistance class is intented to be where the user gets the distance measurements from.
    (It is the counterpart to the LidarDistance class).

---
+++ SensorVehicle-main/VehicleEquipment/DistanceMeasurement/Ultrasound/UltrasoundDistance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleEquipment.DistanceMeasurement.Ultrasound
+{
+    public class UltrasoundDistance : IDistance
+    {
+        public float Fwd { get; private set; }
+        public float Left { get; private set; }
+        public float Right { get; private set; }
+        public float MinRange { get; private set; }
+        public float MaxRange { get; private set; }
+        public float Resolution { get; private set; }
+
+        //TODO
+    }
+}
---
+++ SensorVehicle-main/VehicleEquipment/DistanceMeasurement/Ultrasound/UltrasoundReceiver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleEquipment.DistanceMeasurement.Ultrasound
+{
+    public static class UltrasoundReceiver
+    {
+        //TODO
+    }
+}
