223587870344c5abdfb28299cb8c45c3e603642e
Erlend Vevle Lone <150141@stud.hvl.no>
2019-02-07 12:31:40 +0100

    Adds interface for VehicleEquipementCommunication

---
+++ SensorVehicle-main/VehicleEquipment/IVehicleCommunication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VehicleEquipment
+{
+    public interface IVehicleCommunication
+    {
+        void Write(byte[] data);
+
+        byte[] Read();
+    }
+}
+
