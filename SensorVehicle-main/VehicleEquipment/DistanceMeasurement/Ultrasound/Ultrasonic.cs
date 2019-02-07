cc5d085b9975d048beab473b774634650d812a02
Erlend Vevle Lone <150141@stud.hvl.no>
2019-02-07 12:35:37 +0100

    Adds Ultrasonic class (copied from test repo). Edits made from original: Construtor has interface as parameter. Also deletes unnecessary empty classes.

---
+++ SensorVehicle-main/VehicleEquipment/DistanceMeasurement/Ultrasound/Ultrasonic.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VehicleEquipment.DistanceMeasurement.Ultrasound
+{
+    class Ultrasonic
+    {
+        private readonly IVehicleCommunication vehicleCommunication;
+
+        public Ultrasonic(IVehicleCommunication comWithUltrasonic)
+        {
+            vehicleCommunication = comWithUltrasonic;
+
+        }
+        public string DistanceLeft()
+        {
+            return Distance()[0];
+        }
+
+        public string DistanceForward()
+        {
+            return Distance()[1];
+        }
+
+        public string DistanceRight()
+        {
+            return Distance()[2];
+        }
+
+        private string[] Distance()
+        {
+            byte[] bytes = new byte[20];
+            string text = "";
+            bytes = vehicleCommunication.Read();
+            foreach (byte b in bytes)
+            {
+                text = text + (char)b;
+            }
+
+            return text.Split('-');
+        }
+    }
+}
