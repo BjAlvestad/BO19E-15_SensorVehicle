fd9801703852de2fb364c4a2e3d525f798e1b8a1
BjAlvestad <40029656+BjAlvestad@users.noreply.github.com>
2019-02-08 15:17:54 +0100

    Adds IoC-containers in App.xaml.cs for when on IoT
    Registers the containers that is to be used when code is detected as running on an IoT device (i.e. the physical car).
    
    Note:
    This code has not been tested yet, as that requires the actual car (and more code), neither of which was available at the time of this committ.

--- SensorVehicle-main/Application/App.xaml.cs
+++ SensorVehicle-main/Application/App.xaml.cs
@@ -17,6 +17,13 @@ using Windows.ApplicationModel.Resources;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
+using Communication.Vehicle;
+using VehicleEquipment;
+using VehicleEquipment.DistanceMeasurement.Lidar;
+using VehicleEquipment.DistanceMeasurement.Ultrasound;
+using VehicleEquipment.Locomotion.Encoder;
+using VehicleEquipment.Locomotion.Wheels;
+
 namespace Application
 {
     [Windows.UI.Xaml.Data.Bindable]
@@ -32,7 +39,34 @@ namespace Application
             // register a singleton using Container.RegisterType<IInterface, Type>(new ContainerControlledLifetimeManager());
             base.ConfigureContainer();
             Container.RegisterInstance<IResourceLoader>(new ResourceLoaderAdapter(new ResourceLoader()));
-            Container.RegisterType<ISampleDataService, SampleDataService>();
+            Container.RegisterType<ISampleDataService, SampleDataService>();  //TEMP: Remove after StudentLogic and ExampleLogic pages has been changed
+
+            bool isRunningOnPhysicalCar = Windows.System.Profile.AnalyticsInfo.VersionInfo.DeviceFamily == "Windows.IoT";
+            bool runAgainstSimulator = false;
+
+            ILidarPacketReceiver lidarPacketReceiver;
+            IVehicleCommunication ultrasonicCommunication;
+            IVehicleCommunication encoderCommunication;
+            IVehicleCommunication wheelCommunication;
+            if (isRunningOnPhysicalCar || true)  //TODO: Remove '|| true' once lidarPacketReceiver and the communication interfaces are assigned in the else block
+            {
+                lidarPacketReceiver = new LidarPacketReceiver();
+                ultrasonicCommunication = new VehicleCommunication(Device.Ultrasonic);
+                encoderCommunication = new VehicleCommunication(Device.Encoder);
+                wheelCommunication = new VehicleCommunication(Device.Wheel);
+            }
+            //else if (runAgainstSimulator)
+            //{
+            //    // TODO: Configure for communication against simulator (after SimulatedVehicleEquipment class is created)
+            //}
+            //else // Connect up against mock/random data instead of simulator
+            //{
+
+            //}
+            Container.RegisterType<ILidarDistance, LidarDistance>(new ContainerControlledLifetimeManager(), new InjectionConstructor(lidarPacketReceiver, new VerticalAngle[] { VerticalAngle.Up1, VerticalAngle.Up3 }));
+            Container.RegisterType<IUltrasonic, Ultrasonic>(new ContainerControlledLifetimeManager(), new InjectionConstructor(ultrasonicCommunication));
+            Container.RegisterType<IEncoder, Encoder>(new ContainerControlledLifetimeManager(), new InjectionConstructor(encoderCommunication));
+            Container.RegisterType<IWheel, Wheel>(new ContainerControlledLifetimeManager(), new InjectionConstructor(wheelCommunication));
         }
 
         protected override async Task OnLaunchApplicationAsync(LaunchActivatedEventArgs args)
