1e70d690ce6d925cf746ae81bac1334bb2ee9a4f
BjAlvestad <40029656+BjAlvestad@users.noreply.github.com>
2019-02-08 11:55:19 +0100

    Adds VehicleCommunication class and Device enum
    The intended use for this class (implementing the IVehicleCommunication interface) is to get injected into the equipment classes (Encoder, Wheel etc.) from the main Application at runtime, if the App is running on an IOT device (i.e. the physical sensor vehicle).
    
    Class and enum was copied from TestRepo.
    Branch:    Master
    Solution:  UWP
    File:          VehicleCommunication.cs

---
+++ SensorVehicle-main/Communication/Vehicle/Device.cs
@@ -0,0 +1,10 @@
+﻿namespace Communication.Vehicle
+{
+    internal enum Device
+    {
+        Wheel = 0x20,
+        Ultrasonic = 0x25,
+        Encoder = 0x30,
+        GyroAccelerometer = 0x35
+    }
+}
---
+++ SensorVehicle-main/Communication/Vehicle/VehicleCommunication.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Windows.Devices.I2c;
+using VehicleEquipment;
+
+namespace Communication.Vehicle
+{
+    public class VehicleCommunication : IVehicleCommunication
+    {
+        private I2cDevice _device;
+
+        internal VehicleCommunication(Device address)
+        {
+            SetUpCommunication(address);
+        }
+
+        private async void SetUpCommunication(Device address)
+        {
+            var settings = new I2cConnectionSettings((byte)address)  // Slave Address of Arduino Uno 
+            {
+                BusSpeed = I2cBusSpeed.FastMode // this bus has 400Khz speed
+            };
+            string advancedQuerySyntaxString = I2cDevice.GetDeviceSelector("I2C1"); // This will return Advanced Query String which is used to select i2c device
+            var deviceInformationCollection = await Windows.Devices.Enumeration.DeviceInformation.FindAllAsync(advancedQuerySyntaxString);
+            _device = await I2cDevice.FromIdAsync(deviceInformationCollection[0].Id, settings);
+        }
+
+        public void Write(byte[] data)
+        {
+            _device.Write(data);
+        }
+
+        public byte[] Read()
+        {
+            byte[] data = new byte[20];
+            _device.Read(data);
+
+            return data;
+        }
+    }
+}
