9c4fca5217ce3370fad5be4dae1dea5c33320bde
BjAlvestad <40029656+BjAlvestad@users.noreply.github.com>
2019-02-11 17:08:15 +0100

    Adds View and ViewModel for Wheels (early version)
    There is currently no exceptionhandling (INotifyDataErrorInfo etc.).
    But it seems to be working against the Mock data object.
    
    So it is committed in its current (functionall but non-robust) state, and will be completed later.

--- SensorVehicle-main/Application/ViewModels/WheelsViewModel.cs
+++ SensorVehicle-main/Application/ViewModels/WheelsViewModel.cs
@@ -1,13 +1,114 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Prism.Windows.Mvvm;
+using Prism.Windows.Navigation;
+using VehicleEquipment.Locomotion.Wheels;
 
 namespace Application.ViewModels
 {
     public class WheelsViewModel : ViewModelBase
     {
-        public WheelsViewModel()
+        private CancellationTokenSource _periodicRaisePropertyChangedToken;
+
+        public WheelsViewModel(IWheel wheel)
         {
+            Wheel = wheel;
+        }
+
+        public IWheel Wheel { get; set; }
+
+        private TimeSpan _updateInterval;
+        public TimeSpan UpdateInterval
+        {
+            get { return _updateInterval; }
+            set { SetProperty(ref _updateInterval, value); }
+        }
+
+        private int _leftWheel;
+        public int LeftWheel
+        {
+            get { return _leftWheel; }
+            set
+            {
+                SetProperty(ref _leftWheel, value);
+            }
+        }
+
+        private int _rightWheel;
+        public int RightWheel
+        {
+            get { return _rightWheel; }
+            set
+            {
+                SetProperty(ref _rightWheel, value);
+            }
+        }
+
+        public void ApplyNewWheelSpeed()
+        {
+            Wheel.SetSpeed(LeftWheel, RightWheel);
+            RaisePropertyChanged(nameof(Wheel));
+        }
+
+        private bool _applyWheelSpeedContinously;
+        public bool ApplyWheelSpeedContinously
+        {
+            get { return _applyWheelSpeedContinously; }
+            set
+            {
+                SetProperty(ref _applyWheelSpeedContinously, value);
+                if (TogglePeriodicRaisePropertyChanged == false) TogglePeriodicRaisePropertyChanged = true;
+            }
+        }
+
+        private bool _togglePeriodicRaisePropertyChanged;
+        public bool TogglePeriodicRaisePropertyChanged
+        {
+            get { return _togglePeriodicRaisePropertyChanged; }
+            set
+            {
+                bool valueChanged = SetProperty(ref _togglePeriodicRaisePropertyChanged, value);
+                if (value && valueChanged)
+                {
+                    _periodicRaisePropertyChangedToken = new CancellationTokenSource();
+                    PeriodicRaisePropertyChangedAsync(_periodicRaisePropertyChangedToken.Token);
+                }
+                else if(valueChanged)
+                {
+                    _periodicRaisePropertyChangedToken.Cancel();
+                }
+            }
+        }
+
+        private async Task PeriodicRaisePropertyChangedAsync(CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+                if (ApplyWheelSpeedContinously)
+                {
+                    Wheel.SetSpeed(LeftWheel, RightWheel);
+                }
+
+                RaisePropertyChanged(nameof(Wheel));
+                await Task.Delay(UpdateInterval, cancellationToken);
+            }
+        }
+
+        public override void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
+        {
+            base.OnNavigatedTo(e, viewModelState);
+            LeftWheel = Wheel.CurrentSpeedLeft;
+            RightWheel = Wheel.CurrentSpeedRight;
+            UpdateInterval = TimeSpan.FromMilliseconds(300);
+        }
+
+        public override void OnNavigatingFrom(NavigatingFromEventArgs e, Dictionary<string, object> viewModelState, bool suspending)
+        {
+            ApplyWheelSpeedContinously = false;
+            _periodicRaisePropertyChangedToken.Cancel();
+            base.OnNavigatingFrom(e, viewModelState, suspending);
         }
     }
 }
