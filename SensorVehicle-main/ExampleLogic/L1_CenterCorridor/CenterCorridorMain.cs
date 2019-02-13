05f9a8d47f1dcf426cccc390f3067975214f8cd8
BjAlvestad <40029656+BjAlvestad@users.noreply.github.com>
2019-02-13 15:02:42 +0100

    Adds ExampleLogic template for CenterCorridor
    The actual controll logic (which should reside in Run() method) has not yet been implemented.

---
+++ SensorVehicle-main/ExampleLogic/L1_CenterCorridor/CenterCorridorMain.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using VehicleEquipment.DistanceMeasurement.Ultrasound;
+using VehicleEquipment.Locomotion.Wheels;
+
+namespace ExampleLogic.L1_CenterCorridor
+{
+    public class CenterCorridorMain : IExampleLogic
+    {
+        private readonly IWheel _wheels;
+        private readonly IUltrasonic _ultrasonic;
+
+        public ExampleLogicDetails Details { get; }
+
+        public CenterCorridorMain(IWheel wheels, IUltrasonic ultrasonic)
+        {
+            Details = new ExampleLogicDetails
+            {
+                Title = "L1 - Keep Center of Coridor",
+                Author = "BO19-E15",
+                SuitableForSubjects = "Control systems",
+
+                Description = "Simple cross-connected feedback loop between distance sensor and wheel causes the vehicle to keep to the center of the corridor.\n" +
+                              "The left wheel speed is controlled by the distance from the right ultrasound sensor.\n" +
+                              "The right wheel speed is controlled by the distance from the left ultrasound sensor."
+            };
+
+            _wheels = wheels;
+            _ultrasonic = ultrasonic;
+        }
+
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
