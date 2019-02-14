67512a690dd2d36be6f1a5425d41c1c4f13095e2
Benjamin Odland Skare <benjamin.skare@trisense.no>
2019-02-14 12:48:12 +0100

    Basic wheel control

--- Microcontroller/ARD_Wheels/ARD_Wheels.ino
+++ Microcontroller/ARD_Wheels/ARD_Wheels.ino
@@ -10,11 +10,14 @@ const int safety_pin = 4;
 const int motor_controller_switch = 5;
 const int address = 0x20;
 
-const int full_reverse_data = 28;
-const int full_forward_data = 228;
-const int full_reverse_pwm = 1000;
-const int full_forward_pwm = 2000;
-const int standstill_pwm = 1500;
+const int data_full_reverse = 28;
+const int data_full_forward = 228;
+const int pwm_start_reverse = 1330;
+const int pwm_full_reverse = 1000;
+const int pwm_start_forward = 1600;
+const int pwm_full_forward = 2000;
+const int pwm_standstill = 1500;
+const int ramp_up = 20;
 
 Servo wheels_right;
 Servo wheels_left;
@@ -33,6 +36,8 @@ int set_speed(int new_speed, int speed);
 int data_check(int data);
 int mode_check(int data);
 
+int speed_map(int data);
+
 void setup()
 {
 	Serial.begin(9600);
@@ -45,92 +50,46 @@ void setup()
 
 	wheels_right.attach(right_pwm_pin);
 	wheels_left.attach(left_pwm_pin);
-	wheels_left.writeMicroseconds(standstill_pwm);
-	wheels_right.writeMicroseconds(standstill_pwm);
+	wheels_left.writeMicroseconds(pwm_standstill);
+	wheels_right.writeMicroseconds(pwm_standstill);
 	Wire.onReceive(receive_event);
 	Wire.onRequest(request_event);
 
-	const bool two = digitalRead(mode_pin_2);
-	const bool on = digitalRead(mode_pin_1);
-	if (!on && !two)
-	{
-		mode = 1;
-	}
-	else if (on && !two)
-	{
-		mode = 2;
-	}
-	else if(!on && two)
-	{
-		mode = 3;
-	}
 	Serial.print("MODE: ");
-	mode = 2;
+	mode = 1;
 	Serial.println(mode);
 	Serial.println(digitalRead(safety_pin));
-
-	/*for (int i = 1500; i< 2000; i++)
-	{
-		wheels_left.writeMicroseconds(i);
-	}
-	for (int i = 2000; i > 1500; i--)
-	{
-		wheels_left.writeMicroseconds(i);
-	}*/
-
-
+	
+	speed_left = pwm_standstill;
+	speed_right = pwm_standstill;
+	new_speed_left = pwm_standstill;
+	new_speed_right = pwm_standstill;
 }
 
 void loop()
 {
-	if (wheels_left.readMicroseconds() == standstill_pwm && wheels_right.readMicroseconds() == standstill_pwm)
-	{
-		standstill = true;
-		digitalWrite(motor_controller_switch, LOW);
-	}
+	wheels_right.writeMicroseconds(new_speed_right);
+	wheels_left.writeMicroseconds(new_speed_left);
 
-	if (standstill)
-	{
-		digitalWrite(motor_controller_switch, LOW);
-	}
 
-	safe = digitalRead(safety_pin);
-	if (!safe)
-	{
-		digitalWrite(motor_controller_switch, LOW);
-		wheels_right.writeMicroseconds(standstill_pwm);
-		wheels_left.writeMicroseconds(standstill_pwm);
-		standstill = true;
-	}
-	if (standstill && safe && new_value)
-	{
-		digitalWrite(motor_controller_switch, HIGH);
-		speed_left = set_speed(new_speed_left, speed_left);
-		speed_right = set_speed(new_speed_right, speed_right);
-		wheels_right.writeMicroseconds(speed_right);
-		wheels_left.writeMicroseconds(speed_left);
-		new_value = false;
-		standstill = false;
-	}
-	if (!standstill && safe && new_value)
-	{
-		speed_left = set_speed(new_speed_left, speed_left);
-		speed_right = set_speed(new_speed_right, speed_right);
-		wheels_right.writeMicroseconds(speed_right);
-		wheels_left.writeMicroseconds(speed_left);
-		new_value = false;
-		standstill = false;
-	}
-	/*wheels_right.writeMicroseconds(speed_right);
-	wheels_left.writeMicroseconds(speed_left);*/
+	//wheels_right.writeMicroseconds(speed_right);
+	//wheels_left.writeMicroseconds(speed_left);
+	
+
 	speed_left = set_speed(new_speed_left, speed_left);
 	speed_right = set_speed(new_speed_right, speed_right);
-	wheels_right.writeMicroseconds(speed_right);
-	wheels_left.writeMicroseconds(speed_left);
-	Serial.print("   Left: ");
-	Serial.println(speed_left); 
-	Serial.print("Right: ");
-	Serial.print(speed_right);
+
+
+
+	Serial.print("Left: ");
+	Serial.print(wheels_left.readMicroseconds()); 
+	Serial.print("  Right: ");
+	Serial.println(wheels_right.readMicroseconds());
+	Serial.println(new_speed_left);
+	Serial.println(speed_left);
+	
+
+	//delay(1000);
 	
 
 }
@@ -153,6 +112,7 @@ void receive_event(int x)
 
 	if (Wire.available())
 	{
+
 		data[0] = Wire.read();
 		data[1] = Wire.read();
 		data[2] = Wire.read();
@@ -167,19 +127,39 @@ void receive_event(int x)
 		int data_left = data_check(data[1]);  // Between 28 and 228;
 		int data_right = data_check(data[2]);
 
-		Serial.println(data_left);
-
 		data_left = mode_check(data_left);  // Divide value with mode
 		data_right = mode_check(data_right);
-
 		Serial.println(data_left);
 
-		new_speed_left = map(data_left, full_reverse_data, full_forward_data, full_reverse_pwm, full_forward_pwm);
-		new_speed_right = map(data_right, full_reverse_data, full_forward_data, full_reverse_pwm, full_forward_pwm);
+		new_speed_left = speed_map(data_left);
+		new_speed_right = speed_map(data_right);
+		
+	}
+}
 
-		Serial.println(new_speed_left);
+int speed_map(int data)
+{
+	if (data > 128)
+	{
+		Serial.println(data);
+		data = data - 128;
+		Serial.println(data);
 
-		new_value = true;
+		data = map(data, 0, 100, pwm_start_forward, pwm_full_forward);
+		Serial.println(data);
+
+		return data;
+	}
+	else if (data < 128)
+	{
+
+		data = data - 28;
+		data = map(data, 100, 0, pwm_start_reverse, pwm_full_reverse);
+		return data;
+	}
+	else
+	{
+		return pwm_standstill;
 	}
 }
 
@@ -188,13 +168,13 @@ int set_speed(const int new_speed, int speed)
 
 	if (new_speed != speed)
 	{
-		if (new_speed > standstill_pwm + 20)
+		if (new_speed > pwm_standstill)
 		{
 			// Forward
 			if (new_speed > speed)
 			{
 				// Increase speed
-				speed++;
+				speed+= ramp_up;
 			}
 			else
 			{
@@ -202,13 +182,13 @@ int set_speed(const int new_speed, int speed)
 				speed = new_speed;
 			}
 		}
-		else if (new_speed < standstill_pwm - 20)
+		else if (new_speed < pwm_standstill)
 		{
 			// Backward
 			if (new_speed < speed)
 			{
 				// Increase speed
-				speed--;
+				speed-=ramp_up;
 			}
 			else
 			{
@@ -218,7 +198,7 @@ int set_speed(const int new_speed, int speed)
 		}
 		else
 		{
-			speed = standstill_pwm;
+			speed = pwm_standstill;
 			standstill = true;
 		}
 	}
