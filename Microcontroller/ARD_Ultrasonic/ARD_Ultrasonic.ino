4b3c3a333b8461fef31386ac160fe50b6703a802
Erlend Vevle Lone <150141@stud.hvl.no>
2019-02-19 14:05:21 +0100

    Implements transmission standardization to agree with changes done in Sensorvehicle-main

--- Microcontroller/ARD_Ultrasonic/ARD_Ultrasonic.ino
+++ Microcontroller/ARD_Ultrasonic/ARD_Ultrasonic.ino
@@ -11,12 +11,13 @@ const int echoPinRight = 7;
 const int trigPinForward = 4;
 const int echoPinForward = 5;
 
-const byte ADDRESS = 0x25;
+const int size_of_byte_array = 23;
+const byte address = 0x25;
 
 // defines variables
-String distanceLeft;
-String distanceRight;
-String distanceForward;
+long distanceLeft;
+long distanceRight;
+long distanceForward;
 
 byte bDistanceLeft;
 byte bDistanceRight[2];
@@ -34,7 +35,7 @@ char cR[3];
 char a[20];
 int x;
 void I2CRequest();
-String Ultrasonic(int trigPin, int echoPin);
+long Ultrasonic(int trigPin, int echoPin);
 // the setup function runs once when you press reset or power the board
 void setup() {
 	pinMode(trigPinLeft, OUTPUT); // Sets the trigPin as an Output
@@ -47,7 +48,7 @@ void setup() {
 
 	Serial.begin(9600); // Starts the serial communication
 
-	Wire.begin(ADDRESS); // Set the address of this slave on I2C
+	Wire.begin(address); // Set the address of this slave on I2C
 
 	Wire.onRequest(I2CRequest);
 }
@@ -56,67 +57,25 @@ void setup() {
 void loop() {
 
 	distanceLeft = Ultrasonic(trigPinLeft, echoPinLeft);
-	//Sdl = static_cast<String>(distanceLeft);
-
-	/*distanceLeft.toCharArray(cL, 4);
-	for (char c_l : cL)
-	{
-		Serial.print(c_l);
-	}
-	Serial.println();*/
 	delay(60);
 
 	distanceForward = Ultrasonic(trigPinForward, echoPinForward);
-	//Sdf = static_cast<String>(distanceForward);
-	/*distanceForward.toCharArray(cF, 4);
-	for (char c_l : cF)
-	{
-		Serial.print(c_l);
-	}
-	Serial.println();*/
 	delay(60);
 
 	distanceRight = Ultrasonic(trigPinRight, echoPinRight);
-	//Sdr = static_cast<String>(distanceRight);
-	/*distanceRight.toCharArray(cR, 4);
-	for (char c_l : cR)
-	{
-		Serial.print(c_l);
-	}
-	Serial.println();*/
 	delay(60);
-
-	
-
-
-	s = distanceLeft +"-" + distanceForward + "-" + distanceRight+ "-";
-	Serial.println(s);
-	
-	x = s.length()+1;
-
-	for (char a1 : a)
-	{
-		a1 = ' ';
-	}
-
-	s.toCharArray(a, x);
-	for (char c : a)
-	{
-		Serial.print(c);
-	}
-	Serial.println();
-
-	delay(10);
 }
 
 // function that executes whenever data is requested by master
 // this function is registered as an event, see setup()
 void I2CRequest() {
 	Serial.println("I2C-Request");
-	Wire.write(a); // return data to PI
+	long longsToBeSent[] = { distanceLeft, distanceForward, distanceRight };
+	int arrayLength = sizeof(longsToBeSent) / sizeof(long);
+	SendByteArray(0, arrayLength, longsToBeSent);
 }
 
-String Ultrasonic(int trigPin, int echoPin )
+long Ultrasonic(int trigPin, int echoPin )
 {
 	digitalWrite(trigPin, LOW);
 	delayMicroseconds(2);
@@ -133,17 +92,38 @@ String Ultrasonic(int trigPin, int echoPin )
 
 	if (distance > 400)
 	{
-		return "n+n";
+		return 400;
 	}
 	else if (distance < 2)
 	{
-		return "n-n";
+		return 0;
 	}
 	else
 	{
-		return static_cast<String>(distance);
+		return distance;
+	}	
+}
+
+void SendByteArray(int message, int arrayLength, long longsToBeSent[])
+{
+	byte byteArray[size_of_byte_array];
+	byteArray[0] = (byte)address;
+	byteArray[1] = (byte)message;
+	byteArray[2] = (byte)(arrayLength);
+	int elementsBeforeLong = 3;
+
+	const int bitSizeOfLong = sizeof(long) * 8;
+
+	for (int i = 0; i < arrayLength; i++)
+	{
+
+		int shiftByLong = sizeof(long) * i;
+		for (int j = 1; j <= sizeof(long); j++)
+		{
+			byteArray[(elementsBeforeLong - 1) + shiftByLong + j] = (byte)(longsToBeSent[i] >> (bitSizeOfLong - 8 * j));
+		}
 	}
 
-	
+	Wire.write(byteArray, size_of_byte_array);
 }
 
