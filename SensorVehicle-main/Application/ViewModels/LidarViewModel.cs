f2adf502f1c5b25de8d1284363c0379ab2c51243
BjAlvestad <40029656+BjAlvestad@users.noreply.github.com>
2019-02-25 14:33:58 +0100

    Exposes Lidar and Power in LidarViewModel

--- SensorVehicle-main/Application/ViewModels/LidarViewModel.cs
+++ SensorVehicle-main/Application/ViewModels/LidarViewModel.cs
@@ -1,13 +1,20 @@
 ﻿using System;
-
+using Communication;
 using Prism.Windows.Mvvm;
+using VehicleEquipment.DistanceMeasurement.Lidar;
 
 namespace Application.ViewModels
 {
     public class LidarViewModel : ViewModelBase
     {
-        public LidarViewModel()
+        public LidarViewModel(ILidarDistance lidar, IPower power)
         {
+            Lidar = lidar;
+            Power = power;
         }
+
+        public ILidarDistance Lidar { get; private set; }
+
+        public IPower Power { get; private set; }
     }
 }
