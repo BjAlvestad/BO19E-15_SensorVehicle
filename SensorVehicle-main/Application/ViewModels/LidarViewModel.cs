e8eec136b439bfd7f4e928f4454c9aa92c53a1b1
BjAlvestad <40029656+BjAlvestad@users.noreply.github.com>
2019-02-27 16:04:15 +0100

    Adds preliminary (functioning) LidarView and VM
    It has been tested against mock data, but not against the actual Lidar yet.
    
    The functions seem to be working ok. But it will have to be made more robust later on (especially textbox input), and the layout cleaned up a bit.

--- SensorVehicle-main/Application/ViewModels/LidarViewModel.cs
+++ SensorVehicle-main/Application/ViewModels/LidarViewModel.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.UI.Xaml.Data;
 using Communication;
 using Prism.Windows.Mvvm;
+using Prism.Windows.Navigation;
 using VehicleEquipment.DistanceMeasurement.Lidar;
 
 namespace Application.ViewModels
@@ -11,10 +15,103 @@ namespace Application.ViewModels
         {
             Lidar = lidar;
             Power = power;
+            FromAngle = 255;
+            ToAngle = 285;
+            CalculationTypes = new List<CalculationType>(Enum.GetValues(typeof(CalculationType)).Cast<CalculationType>());
+            VerticalAngles = new List<VerticalAngle>(Enum.GetValues(typeof(VerticalAngle)).Cast<VerticalAngle>());
+            ActiveVerticalAngles = new List<VerticalAngle>();
         }
 
-        public ILidarDistance Lidar { get; private set; }
+        public List<VerticalAngle> ActiveVerticalAngles;
 
-        public IPower Power { get; private set; }
+        public ILidarDistance Lidar { get; }
+
+        public IPower Power { get; }
+
+        public List<CalculationType> CalculationTypes { get; }
+
+        private List<VerticalAngle> _verticalAngles;
+        public List<VerticalAngle> VerticalAngles
+        {
+            get { return _verticalAngles; }
+            set { SetProperty(ref _verticalAngles, value); }
+        }
+
+        public VerticalAngle SelectedVerticalAngle { get; set; }
+
+        public VerticalAngle SelectedActiveVerticalAngle
+        {
+            get { return _selectedActiveVerticalAngle; }
+            set { SetProperty(ref _selectedActiveVerticalAngle, value); }
+        }
+
+        public void AddSelectedVerticalAngleToActive()
+        {
+            Lidar.ActiveVerticalAngles.Add(SelectedVerticalAngle);
+        }
+
+        public void RemoveSelectedVerticalAngleFromActive()
+        {
+            if (Lidar.ActiveVerticalAngles.Count > 1 && SelectedActiveVerticalAngle != Lidar.DefaultVerticalAngle)
+            {
+                VerticalAngle angleToRemove = SelectedActiveVerticalAngle;
+                SelectedActiveVerticalAngle = Lidar.DefaultVerticalAngle;
+                Lidar.ActiveVerticalAngles.Remove(angleToRemove);
+            }
+        }
+
+        public void SetAsDefaultAngle()
+        {
+            Lidar.DefaultVerticalAngle = SelectedActiveVerticalAngle;
+        }
+
+        private int _fromAngle;
+        public int FromAngle
+        {
+            get { return _fromAngle; }
+            set { SetProperty(ref _fromAngle, value); }
+        }
+
+        private int _toAngle;
+        private VerticalAngle _selectedActiveVerticalAngle;
+
+        public int ToAngle
+        {
+            get { return _toAngle; }
+            set { SetProperty(ref _toAngle, value); }
+        }
+
+        public List<float> DistancesInRange => Lidar.GetDistancesInRange(FromAngle, ToAngle, Lidar.DefaultVerticalAngle);
+
+        private bool _autocalculate;
+        public bool Autocalculate
+        {
+            get { return _autocalculate; }
+            set { SetProperty(ref _autocalculate, value); }
+        }
+
+        private void Lidar_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName != nameof(Lidar.LastUpdate)) return;
+
+            RaisePropertyChanged(nameof(DistancesInRange));
+            if (!Autocalculate) return;
+
+            float fwd = Lidar.Fwd;
+            float left = Lidar.Left;
+            float right = Lidar.Right;
+        }
+
+        public override void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
+        {
+            base.OnNavigatedTo(e, viewModelState);
+            Lidar.PropertyChanged += Lidar_PropertyChanged;
+        }
+
+        public override void OnNavigatingFrom(NavigatingFromEventArgs e, Dictionary<string, object> viewModelState, bool suspending)
+        {
+            Lidar.PropertyChanged -= Lidar_PropertyChanged;
+            base.OnNavigatingFrom(e, viewModelState, suspending);
+        }
     }
 }
