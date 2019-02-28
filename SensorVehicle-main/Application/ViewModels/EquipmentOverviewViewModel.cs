e003a80f7ecad7d0d7b4d62a347570162e2684a0
BjAlvestad <40029656+BjAlvestad@users.noreply.github.com>
2019-02-28 14:49:57 +0100

    Adds Wheel speed (setpoint) to EquipmentOverview
    Also adds button to stop control logic and then send additional stop command to wheel microcontroller (as an extra precaution).

--- SensorVehicle-main/Application/ViewModels/EquipmentOverviewViewModel.cs
+++ SensorVehicle-main/Application/ViewModels/EquipmentOverviewViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Communication;
+using ExampleLogic;
 using Prism.Windows.Mvvm;
 using Prism.Windows.Navigation;
 using VehicleEquipment.DistanceMeasurement.Lidar;
@@ -12,13 +13,14 @@ namespace Application.ViewModels
 {
     public class EquipmentOverviewViewModel : ViewModelBase
     {
-        public EquipmentOverviewViewModel(IUltrasonic ultrasonic, ILidarDistance lidar, IWheel wheel, IEncoder encoder, IPower power)
+        public EquipmentOverviewViewModel(IUltrasonic ultrasonic, ILidarDistance lidar, IWheel wheel, IEncoder encoder, IPower power, ExampleLogicService exampleLogic)
         {
             Ultrasonic = ultrasonic;
             Lidar = lidar;
             Wheel = wheel;
             Encoder = encoder;
             Power = power;
+            ExampleLogic = exampleLogic;
         }
 
         public IUltrasonic Ultrasonic { get; set; }
@@ -26,12 +28,20 @@ namespace Application.ViewModels
         public IWheel Wheel { get; set; }
         public IEncoder Encoder { get; set; }
         public IPower Power { get; set; }
+        public ExampleLogicService ExampleLogic { get; set; }
 
         private bool _autoCalculateLidarDistance;
         public bool AutoCalculateLidarDistance
         {
             get { return _autoCalculateLidarDistance; }
             set { SetProperty(ref _autoCalculateLidarDistance, value); }
+        }
+
+        public void StopWheels()
+        {
+            if (ExampleLogic.ActiveExampleLogic != null) ExampleLogic.ActiveExampleLogic.RunExampleLogic = false;
+
+            Wheel.SetSpeed(0, 0);
         }
 
         private void Lidar_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
