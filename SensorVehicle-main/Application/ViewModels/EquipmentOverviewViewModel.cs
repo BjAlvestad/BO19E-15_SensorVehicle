b1c4db3f58f05443c8101e5da0077345a3b7b159
BjAlvestad <40029656+BjAlvestad@users.noreply.github.com>
2019-02-28 11:29:32 +0100

    Adds view and VM for Sensor Overview (Lidar & US)
    Lidar and Ultrasound is more or less complete.
    Wheel and encoder data has not been added yet

--- SensorVehicle-main/Application/ViewModels/EquipmentOverviewViewModel.cs
+++ SensorVehicle-main/Application/ViewModels/EquipmentOverviewViewModel.cs
@@ -1,13 +1,60 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using Communication;
 using Prism.Windows.Mvvm;
+using Prism.Windows.Navigation;
+using VehicleEquipment.DistanceMeasurement.Lidar;
+using VehicleEquipment.DistanceMeasurement.Ultrasound;
+using VehicleEquipment.Locomotion.Encoder;
+using VehicleEquipment.Locomotion.Wheels;
 
 namespace Application.ViewModels
 {
     public class EquipmentOverviewViewModel : ViewModelBase
     {
-        public EquipmentOverviewViewModel()
+        public EquipmentOverviewViewModel(IUltrasonic ultrasonic, ILidarDistance lidar, IWheel wheel, IEncoder encoder, IPower power)
         {
+            Ultrasonic = ultrasonic;
+            Lidar = lidar;
+            Wheel = wheel;
+            Encoder = encoder;
+            Power = power;
+        }
+
+        public IUltrasonic Ultrasonic { get; set; }
+        public ILidarDistance Lidar { get; set; }
+        public IWheel Wheel { get; set; }
+        public IEncoder Encoder { get; set; }
+        public IPower Power { get; set; }
+
+        private bool _autoCalculateLidarDistance;
+        public bool AutoCalculateLidarDistance
+        {
+            get { return _autoCalculateLidarDistance; }
+            set { SetProperty(ref _autoCalculateLidarDistance, value); }
+        }
+
+        private void Lidar_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName != nameof(Lidar.LastUpdate)) return;
+
+            if (!AutoCalculateLidarDistance) return;
+
+            float fwd = Lidar.Fwd;
+            float left = Lidar.Left;
+            float right = Lidar.Right;
+        }
+
+        public override void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
+        {
+            base.OnNavigatedTo(e, viewModelState);
+            Lidar.PropertyChanged += Lidar_PropertyChanged;
+        }
+
+        public override void OnNavigatingFrom(NavigatingFromEventArgs e, Dictionary<string, object> viewModelState, bool suspending)
+        {
+            Lidar.PropertyChanged -= Lidar_PropertyChanged;
+            base.OnNavigatingFrom(e, viewModelState, suspending);
         }
     }
 }
