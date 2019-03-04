ded7e8658bf969937a02a14c41a06cc9c60aa423
BjAlvestad <40029656+BjAlvestad@users.noreply.github.com>
2019-03-04 09:38:00 +0100

    Adds error message properties and methods
    The HasUnacknowledgedError property will be used used for putting a red frame arround the shell button when an error is active, and from preventing re-start of control logic until error has been acknowledged.
    
    ClearMessgage() method will be used for removing the message stored in ErrorMessage property, and to allow the control logic to be started again.

--- SensorVehicle-main/ExampleLogic/ExampleLogicBase.cs
+++ SensorVehicle-main/ExampleLogic/ExampleLogicBase.cs
@@ -15,6 +15,13 @@ namespace ExampleLogic
         /// </summary>
         public abstract ExampleLogicDetails Details { get; }
 
+        private bool _hasUnacknowledgedError;
+        public bool HasUnacknowledgedError
+        {
+            get { return _hasUnacknowledgedError; }
+            set { SetProperty(ref _hasUnacknowledgedError, value); }
+        }
+
         private string _errorMessage;
         public string ErrorMessage
         {
@@ -29,6 +36,12 @@ namespace ExampleLogic
 
         public abstract void Initialize();
         public abstract void Run();
+
+        public void ClearMessage()
+        {
+            ErrorMessage = "";
+            HasUnacknowledgedError = false;
+        }
 
         private bool _runExampleLogic;
         public bool RunExampleLogic
@@ -50,6 +63,8 @@ namespace ExampleLogic
         {
             Task.Run(() =>
             {
+                if (HasUnacknowledgedError) return;
+
                 try
                 {
                     Initialize();
@@ -61,7 +76,8 @@ namespace ExampleLogic
                 }
                 catch (Exception e)
                 {
-                    _runExampleLogic = false;
+                    RunExampleLogic = false;
+                    HasUnacknowledgedError = true;
                     ErrorMessage = $"CONTROL LOGIC ERROR - '{Details.Title}' generated the following exception:\n{e.Message}";
                     _wheel.SetSpeed(0, 0);
                 }
