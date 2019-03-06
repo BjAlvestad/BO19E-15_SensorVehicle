33c69153ee43bd93e1a4a26fba93e294532cceaf
BjAlvestad <40029656+BjAlvestad@users.noreply.github.com>
2019-03-06 07:28:49 +0100

    Adds empty class for SimulatedLidarPacketReceiver
    Communications with simulator has not yet been implemented.

---
+++ SensorVehicle-main/Communication/Simulator/SimulatedLidarPacketReceiver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using VehicleEquipment.DistanceMeasurement.Lidar;
+
+namespace Communication.Simulator
+{
+    public class SimulatedLidarPacketReceiver : ILidarPacketReceiver
+    {
+        public SimulatedLidarPacketReceiver(SimulatorAppServiceClient simulatorCommunication)
+        {
+            
+        }
+
+        public Task<Queue<byte[]>> GetQueueOfDataPacketsAsync(byte numberOfCycles)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
