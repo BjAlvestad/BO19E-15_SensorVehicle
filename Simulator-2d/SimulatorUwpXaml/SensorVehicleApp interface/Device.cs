6002d10d47ce7cf1afdd92ab4f8a3c7b99ed8cc3
BjAlvestad <40029656+BjAlvestad@users.noreply.github.com>
2019-03-06 09:37:37 +0100

    Copies Device enum from SensorVehicle-Project
    This is intended to be used in conjunction with messages sent via the AppService.

---
+++ Simulator-2d/SimulatorUwpXaml/SensorVehicleApp interface/Device.cs
@@ -0,0 +1,10 @@
+﻿namespace SimulatorUwpXaml
+{
+    public enum Device
+    {
+        Wheel = 0x20,
+        Ultrasonic = 0x25,
+        Encoder = 0x30,
+        GyroAccelerometer = 0x35
+    }
+}
