8b0746d799802e71356380aacd94c1c5ca33f856
BjAlvestad <40029656+BjAlvestad@users.noreply.github.com>
2019-03-06 07:29:41 +0100

    Adds empty class for SimulatedVehicleCommunication
    Communications with simulator has not yet been implemented.

---
+++ SensorVehicle-main/Communication/Simulator/SimulatedVehicleCommunication.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using VehicleEquipment;
+
+namespace Communication.Simulator
+{
+    public class SimulatedVehicleCommunication : IVehicleCommunication
+    {
+        private readonly Device _simulatedDevice;
+        private readonly string _nameOfDevice;
+        private readonly SimulatorAppServiceClient _simulatorCommunication;
+
+        public SimulatedVehicleCommunication(Device deviceToSimulate, SimulatorAppServiceClient simulatorCommunication)
+        {
+            _simulatorCommunication = simulatorCommunication;
+            _simulatedDevice = deviceToSimulate;
+            _nameOfDevice = Enum.GetName(typeof(Device), _simulatedDevice);
+        }
+
+        public void Write(MessageCode message, params int[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VehicleDataPacket Read()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
