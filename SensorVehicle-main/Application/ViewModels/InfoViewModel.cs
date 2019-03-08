16cc1d52b8a27fd300f6fb8651ec24c9e93cdfc4
BjAlvestad <40029656+BjAlvestad@users.noreply.github.com>
2019-03-08 10:10:15 +0100

    Adds info to user on what code is running against
    The info-page will display a textbox with info on whether the code is running on car, or on PC against simulator, or on PC against mock data.

--- SensorVehicle-main/Application/ViewModels/InfoViewModel.cs
+++ SensorVehicle-main/Application/ViewModels/InfoViewModel.cs
@@ -1,13 +1,47 @@
 ﻿using System;
-
+using Windows.System;
+using Prism.Unity.Windows;
 using Prism.Windows.Mvvm;
 
 namespace Application.ViewModels
 {
     public class InfoViewModel : ViewModelBase
     {
+        public bool SimulatorAvailable => ((App) PrismUnityApplication.Current).RunAgainstSimulatorInsteadOfMock == false;
+
         public InfoViewModel()
         {
+
+        }
+
+        public string SimulatorAvailabilityMessage
+        {
+            get
+            {
+                if (((App) PrismUnityApplication.Current).IsRunningOnPhysicalCar)
+                {
+                    return "The code is currently running on the pysical sensor car (IoT) device.\n" +
+                           "Application is connected up against real microcontrollers";
+                }
+                if (((App) PrismUnityApplication.Current).RunAgainstSimulatorInsteadOfMock)
+                {
+                    return "The application is set up to run against simulator.\n" +
+                           "The simulator should have launced automatically. Do not close simulator before this app is closed.\n" +
+                           "\n" +
+                           "Instead of connecting up against real microcontrollers, it will use simulated data from the simulator.\n" +
+                           "You can test run your logic as if you were on the physical car";
+                }
+
+                if (((App) PrismUnityApplication.Current).SimulatorAppAvailabilityStatus == LaunchQuerySupportStatus.AppNotInstalled)
+                {
+                    return "Running against mock data.\n" +
+                           "If you wish to be able to actually test your code without the physical car, you can install our simulator.";
+                    //TODO: Add instruction on where to find, or button which links to the App in the AppStore
+                }
+
+                return $"Code is not running on physical car (IoT device), so tried to look for simulator, but something strange occured.\n" +
+                       $"Simulator availability status: {((App) PrismUnityApplication.Current).SimulatorAppAvailabilityStatus}.";
+            }
         }
     }
 }
