88e50dd3e9907018360ee75f8b2b2ab4fb1b261a
BjAlvestad <40029656+BjAlvestad@users.noreply.github.com>
2019-03-12 18:40:13 +0100

    Modifies ultrasound on simulator to reflect real
    The real ultrasound code has been changed to:
    - 4 sensors (one additional at front).
    - Gives 4.00 if distance measured was over 4 meters.
    - Gives 4.01 if distance measurement timed out (in simulator 4.01 is given if we get NaN, i.e. no walls were detected).

--- Simulator-2d/SimulatorUwpXaml/SensorVehicleApp interface/SimulatedUltrasoundSensor.cs
+++ Simulator-2d/SimulatorUwpXaml/SensorVehicleApp interface/SimulatedUltrasoundSensor.cs
@@ -1,10 +1,13 @@
-﻿using Windows.Foundation.Collections;
+﻿using System;
+using Windows.Foundation.Collections;
 
 namespace SimulatorUwpXaml.SensorVehicleApp_interface
 {
     public class SimulatedUltrasoundSensor
     {
-        private const int NumberOfUltrasoundSensors = 3;
+        private const int NumberOfUltrasoundSensors = 4;
+        private const int MaximumDistanceInCm = 400;
+        private const int TimeOutDistanceInCm = 401;
         private Lidar _distances;
 
         public SimulatedUltrasoundSensor(Lidar distances)
@@ -29,9 +32,14 @@ namespace SimulatorUwpXaml.SensorVehicleApp_interface
         {
             int[] distanceData = new int[NumberOfUltrasoundSensors];
 
-            distanceData[0] = (int)(_distances.Left * 100);
-            distanceData[1] = (int)(_distances.Fwd * 100);
-            distanceData[2] = (int)(_distances.Right * 100);
+            int distanceLeftInCm = float.IsNaN(_distances.Left) ? TimeOutDistanceInCm : Math.Min((int)(_distances.Left * 100), MaximumDistanceInCm);
+            int distanceFwdInCm = float.IsNaN(_distances.Fwd) ? TimeOutDistanceInCm : Math.Min((int)(_distances.Fwd * 100), MaximumDistanceInCm);
+            int distanceRightInCm = float.IsNaN(_distances.Right) ? TimeOutDistanceInCm : Math.Min((int)(_distances.Right * 100), MaximumDistanceInCm);
+
+            distanceData[0] = distanceLeftInCm;
+            distanceData[1] = Math.Min(distanceFwdInCm, MaximumDistanceInCm);  // FwdRight
+            distanceData[2] = distanceRightInCm;
+            distanceData[3] = distanceFwdInCm;  // FwdLeft
 
             return distanceData;
         }
