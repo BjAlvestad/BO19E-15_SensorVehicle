6bce59b2b34aa0b05b25f3554e253a8cf3a56d35
BjAlvestad <40029656+BjAlvestad@users.noreply.github.com>
2019-04-05 13:47:08 +0200

    Improves simulated ultrasound by adding offsets
    Also creates a method for calculating Ultrasound distance based on the measurments in the Simulator.
    (e.g. the real ultrasound sensors on the front will not detect the walls on the side if it is driving perpendicular to it, whereas the simulated ones previously would).
    
    Also adds a TODO reminder to check the behaviours of the real ultrasound sensors, and modify simulator against those measurements.

--- Simulator-2d/SimulatorUwpXaml/SensorVehicleApp interface/SimulatedUltrasoundSensor.cs
+++ Simulator-2d/SimulatorUwpXaml/SensorVehicleApp interface/SimulatedUltrasoundSensor.cs
@@ -6,13 +6,17 @@ namespace SimulatorUwpXaml.SensorVehicleApp_interface
     public class SimulatedUltrasoundSensor
     {
         private const int NumberOfUltrasoundSensors = 4;
-        private const int MaximumDistanceInCm = 400;
-        private const int TimeOutDistanceInCm = 401;
+        private const float MaximumDistance = 4.00f;
+        private const float TimeOutDistance = 4.01f;
+        private readonly float _sensorOffsetFwd;
+        private readonly float _sensorOffsetSide;
         private Lidar _distances;
 
-        public SimulatedUltrasoundSensor(Lidar distances)
+        public SimulatedUltrasoundSensor(Lidar distances, float sensorOffsetFwd, float sensorOffsetSide)
         {
             _distances = distances;
+            _sensorOffsetFwd = sensorOffsetFwd;
+            _sensorOffsetSide = sensorOffsetSide;
         }
 
         public ValueSet ReturnData()
@@ -32,16 +36,34 @@ namespace SimulatorUwpXaml.SensorVehicleApp_interface
         {
             int[] distanceData = new int[NumberOfUltrasoundSensors];
 
-            int distanceLeftInCm = float.IsNaN(_distances.Left) ? TimeOutDistanceInCm : Math.Min((int)(_distances.Left * 100), MaximumDistanceInCm);
-            int distanceFwdInCm = float.IsNaN(_distances.Fwd) ? TimeOutDistanceInCm : Math.Min((int)(_distances.Fwd * 100), MaximumDistanceInCm);
-            int distanceRightInCm = float.IsNaN(_distances.Right) ? TimeOutDistanceInCm : Math.Min((int)(_distances.Right * 100), MaximumDistanceInCm);
+            int distanceLeftInCm = GetUltrasoundDistanceFromLidarData(270, _sensorOffsetFwd);
+            int distanceFwdInCm = GetUltrasoundDistanceFromLidarData(0, _sensorOffsetSide);
+            int distanceRightInCm = GetUltrasoundDistanceFromLidarData(90, _sensorOffsetSide);
 
-            distanceData[0] = distanceLeftInCm;
-            distanceData[1] = Math.Min(distanceFwdInCm, MaximumDistanceInCm);  // FwdRight
-            distanceData[2] = distanceRightInCm;
-            distanceData[3] = distanceFwdInCm;  // FwdLeft
+            distanceData[0] = Math.Max(distanceLeftInCm, 0);
+            distanceData[1] = Math.Max(distanceFwdInCm, 0);  // FwdRight
+            distanceData[2] = Math.Max(distanceRightInCm, 0);
+            distanceData[3] = Math.Max(distanceFwdInCm, 0);  // FwdLeft
 
             return distanceData;
+        }
+
+        private int GetUltrasoundDistanceFromLidarData(int mountingAngle, float sensorOffset)
+        {
+            //TODO: Check how real ultrasound sensors behave, and modify close/long-RangeHalfBeam, and closeRangeMaxDistance to make i as close as possible to real sensor. Also consider adding a midRange if necessary.
+            const int closeRangeHalfBeam = 15;
+            const float closeRangeMaxDistance = 1.00f;
+            const int longRangeHalfBeam = 5;
+
+            float closeRangeDistance = _distances.GetSmallestDistanceInRange(mountingAngle - closeRangeHalfBeam, mountingAngle + closeRangeHalfBeam);
+            float longRangeDistance = _distances.GetSmallestDistanceInRange(mountingAngle - longRangeHalfBeam, mountingAngle + longRangeHalfBeam);
+
+            if (float.IsNaN(closeRangeDistance)) closeRangeDistance = TimeOutDistance;
+            if (float.IsNaN(longRangeDistance)) longRangeDistance = TimeOutDistance;
+
+            float shortestDistanceInCm = (closeRangeDistance > closeRangeMaxDistance) ? longRangeDistance : Math.Min(closeRangeDistance, longRangeDistance) - sensorOffset;
+
+            return (int)(Math.Min(shortestDistanceInCm, MaximumDistance) * 100);
         }
     }
 }
