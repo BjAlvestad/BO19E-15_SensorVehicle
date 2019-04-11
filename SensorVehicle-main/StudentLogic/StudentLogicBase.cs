230c8b467e59490a29ae2dce028da17fa4ee0c50
BjAlvestad <40029656+BjAlvestad@users.noreply.github.com>
2019-04-11 10:58:37 +0200

    Adds refrences and base files for StudentLogic
    The student logic files were copied from the ExampleLogic project, and the variables renamed Student... instead of Example...

---
+++ SensorVehicle-main/StudentLogic/StudentLogicBase.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Helpers;
+using VehicleEquipment.Locomotion.Wheels;
+
+namespace StudentLogic
+{
+    public abstract class StudentLogicBase : ThreadSafeNotifyPropertyChanged
+    {
+        private IWheel _wheel;
+        private CancellationTokenSource _cancellationTokenSource;
+
+        /// <summary>
+        /// Contains the information that will be displayed in the GUI. 
+        /// It should be initialized, and all its fields set to your desired values in the constructor of the child-class.
+        /// </summary>
+        public abstract StudentLogicDescription Details { get; }
+
+        private bool _hasUnacknowledgedError;
+        public bool HasUnacknowledgedError
+        {
+            get { return _hasUnacknowledgedError; }
+            set { SetProperty(ref _hasUnacknowledgedError, value); }
+        }
+
+        private string _errorMessage;
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            private set { SetProperty(ref _errorMessage, value); }
+        }
+
+        protected StudentLogicBase(IWheel wheel)
+        {
+            _wheel = wheel;
+        }
+
+        public abstract void Initialize();
+        public abstract void Run(CancellationToken cancellationToken);
+
+        public void ClearMessage()
+        {
+            ErrorMessage = "";
+            HasUnacknowledgedError = false;
+        }
+
+        private bool _runStudentLogic;
+        public bool RunStudentLogic
+        {
+            get { return _runStudentLogic; }
+            set
+            {
+                if (value == _runStudentLogic) return;
+
+                _runStudentLogic = value;
+                if (value)
+                {
+                    StartControlLogicTask();
+                }
+                else
+                {
+                    _cancellationTokenSource?.Cancel();
+                }
+
+                RaiseSyncedPropertyChanged();
+            }
+        }
+
+        private void StartControlLogicTask()
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+            Task.Run(() =>
+            {
+                if (HasUnacknowledgedError) return;
+
+                try
+                {
+                    Initialize();
+                    while (RunStudentLogic && !_cancellationTokenSource.IsCancellationRequested)
+                    {
+                        Run(_cancellationTokenSource.Token);
+                    }
+                    _wheel.SetSpeed(0, 0);
+                }
+                catch (Exception e)
+                {
+                    RunStudentLogic = false;
+                    HasUnacknowledgedError = true;
+                    ErrorMessage = $"CONTROL LOGIC ERROR - '{Details.Title}' generated the following exception:\n{e.Message}";
+                    _wheel.SetSpeed(0, 0);
+                }
+            }, _cancellationTokenSource.Token);
+        }
+    }
+}
---
+++ SensorVehicle-main/StudentLogic/StudentLogicService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using Helpers;
+using VehicleEquipment.DistanceMeasurement.Lidar;
+using VehicleEquipment.DistanceMeasurement.Ultrasound;
+using VehicleEquipment.Locomotion.Encoder;
+using VehicleEquipment.Locomotion.Wheels;
+
+namespace StudentLogic
+{
+    public class StudentLogicService : ThreadSafeNotifyPropertyChanged
+    {
+        public ObservableCollection<StudentLogicBase> StudentLogics { get; set; }
+
+        private StudentLogicBase _activeStudentLogic;
+        public StudentLogicBase ActiveStudentLogic
+        {
+            get { return _activeStudentLogic; }
+            set { SetProperty(ref _activeStudentLogic, value); }
+        }
+
+        // Any inteface/class registered as a container may be added to the constructor without any further actions
+        public StudentLogicService(IWheel wheels, IEncoders encoders, ILidarDistance lidar, IUltrasonic ultrasonic)
+        {
+            StudentLogics = new ObservableCollection<StudentLogicBase>
+            {
+                // Child classes instatiated in the StudentLogics collection will automatically appear in the GUI
+                // Pass the sensors to be used as arguments (the ones specified in the constructor of the child class).
+                
+            };
+        }
+    }
+}
