26a6fe4a644e94c9b29cd057202b574ccf23f15e
BjAlvestad <40029656+BjAlvestad@users.noreply.github.com>
2019-04-16 12:10:14 +0200

    Uses the new error class in the ControlLogics
    This commit makes the StudentLogic and ErrorLogic cleaner, but does not cause any functional/visual change.

--- SensorVehicle-main/ExampleLogic/ExampleLogicBase.cs
+++ SensorVehicle-main/ExampleLogic/ExampleLogicBase.cs
@@ -16,48 +16,16 @@ namespace ExampleLogic
         /// It should be initialized, and all its fields set to your desired values in the constructor of the child-class.
         /// </summary>
         public abstract ExampleLogicDetails Details { get; }
-
-        private bool _hasUnacknowledgedError;
-        public bool HasUnacknowledgedError
-        {
-            get { return _hasUnacknowledgedError; }
-            set { SetProperty(ref _hasUnacknowledgedError, value); }
-        }
-
-        private string _errorMessage;
-        public string ErrorMessage
-        {
-            get { return _errorMessage; }
-            private set { SetProperty(ref _errorMessage, value); }
-        }
-
-        private string _errorMessageDetails;
-        public string ErrorMessageDetails
-        {
-            get { return _errorMessageDetails; }
-            private set { SetProperty(ref _errorMessageDetails, value); }
-        }
-
-        private bool _showErrorDetails;
-        public bool ShowErrorDetails
-        {
-            get { return _showErrorDetails; }
-            set { SetProperty(ref _showErrorDetails, value); }
-        }
+        public Error Error { get; }
 
         protected ExampleLogicBase(IWheel wheel)
         {
             _wheel = wheel;
+            Error = new Error();
         }
 
         public abstract void Initialize();
         public abstract void Run(CancellationToken cancellationToken);
-
-        public void ClearMessage()
-        {
-            ErrorMessage = "";
-            HasUnacknowledgedError = false;
-        }
 
         private bool _runExampleLogic;
         public bool RunExampleLogic
@@ -86,7 +54,7 @@ namespace ExampleLogic
             _cancellationTokenSource = new CancellationTokenSource();
             Task.Run(() =>
             {
-                if (HasUnacknowledgedError)
+                if (Error.Unacknowledged)
                 {
                     RunExampleLogic = false;
                     return;
@@ -104,9 +72,9 @@ namespace ExampleLogic
                 catch (Exception e)
                 {
                     RunExampleLogic = false;
-                    HasUnacknowledgedError = true;
-                    ErrorMessage = e.Message;
-                    ErrorMessageDetails = e.ToString();
+                    Error.Unacknowledged = true;
+                    Error.Message = e.Message;
+                    Error.DetailedMessage = e.ToString();
                     _wheel.SetSpeed(0, 0);
                 }
             }, _cancellationTokenSource.Token);
