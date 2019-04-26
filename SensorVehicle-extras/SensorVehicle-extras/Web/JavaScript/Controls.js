9473ed252b218fe514f089606d942491f6ddbbb5
Erlend Vevle Lone <150141@stud.hvl.no>
2019-04-26 08:26:28 +0200

    Adds streaming to web page. Code must be simplified

--- SensorVehicle-extras/SensorVehicle-extras/Web/JavaScript/Controls.js
+++ SensorVehicle-extras/SensorVehicle-extras/Web/JavaScript/Controls.js
@@ -1,1 +1,5 @@
-﻿
+﻿var showControls = document.location.pathname !== undefined ? document.location.pathname.toLowerCase().startsWith("/control") : false;
+
+if (showControls === true) {
+    document.getElementById("video-settings").classList.remove("video-settings-hide");
+}
