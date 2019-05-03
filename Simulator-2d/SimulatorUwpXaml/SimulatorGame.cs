6e84daec0d5d98aae1120f557239b10d2e6db305
BjAlvestad <40029656+BjAlvestad@users.noreply.github.com>
2019-05-03 10:50:39 +0200

    Reduces traget FPS and distance update interval
    - Target FPS was set to 30FPS (1/30.0 seconds).
    - Checks age for lidar/distance reading prior to updating it again.
    
    These changes were done to reduce CPU usage.

--- Simulator-2d/SimulatorUwpXaml/SimulatorGame.cs
+++ Simulator-2d/SimulatorUwpXaml/SimulatorGame.cs
@@ -1,4 +1,5 @@
-﻿using Windows.Graphics.Display;
+﻿using System;
+using Windows.Graphics.Display;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Comora;
@@ -30,6 +31,8 @@ namespace SimulatorUwpXaml
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            //IsFixedTimeStep = false;  // Setting this to false will cause next frame to render as soon as the previous is complete (default is true where it will occur at a fixed interval). https://stackoverflow.com/a/8554106/1821428
+            TargetElapsedTime = TimeSpan.FromSeconds(1 / 30.0);  // Sets target/max frame rate. Default is 60FPS.
         }
 
         /// <summary>
@@ -90,7 +93,11 @@ namespace SimulatorUwpXaml
         {
             float elapsedTimeSinceLastUpdate = (float)gameTime.ElapsedGameTime.TotalSeconds; // Get time elapsed since last Update iteration
             _vehicle.Update(elapsedTimeSinceLastUpdate, WallClearanceOk(0.1f));
-            _lidar.Update360(_simulatorMap.Boundaries, Screen.ScaleToHighDPI(1.0f * GlobalScale));
+
+            if (DateTime.Now - _lidar.DistanceReadingAge > TimeSpan.FromMilliseconds(100))
+            {
+                _lidar.Update360(_simulatorMap.Boundaries, Screen.ScaleToHighDPI(1.0f * GlobalScale));
+            }
 
             SetCameraZoom(gameTime);
             _camera.Update(gameTime);
