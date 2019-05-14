c954346ebcee450a716aec061fb02e159e146b4c
Benjamin Odland Skare <benjamin.skare@outlook.com>
2019-05-14 11:04:41 +0200

    Image simulator updated

--- Simulator-2d/SimulatorUwpXaml/SimulatorGame.cs
+++ Simulator-2d/SimulatorUwpXaml/SimulatorGame.cs
@@ -60,7 +60,7 @@ namespace SimulatorUwpXaml
 
             _simulatorMap = new SimulatorMap(Content, mapName: "RealMapFinite.tmx", scale: Screen.ScaleToHighDPI(1.0f * GlobalScale));
 
-            Texture2D carTexture = Content.Load<Texture2D>("SpriteImages/SensorVehicle_Prototype2-300x316");
+            Texture2D carTexture = Content.Load<Texture2D>("SpriteImages/SensorVehicleTop_300x300");
             _vehicle = new VehicleSprite(GraphicsDevice, carTexture, Screen.ScaleToHighDPIWithoutRounding(0.1f * GlobalScale));
 
             _vehicle.Position = _simulatorMap.VehicleStartPosition;
